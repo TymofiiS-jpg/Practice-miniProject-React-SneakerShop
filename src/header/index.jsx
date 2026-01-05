e55638bff75bd9cbca044a817f8b58cd7fe6c271
TymofiiS-jpg <soloachenko005@icloud.com>
2026-01-05 19:37:58 +0100

    Setup

---
+++ src/header/index.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div>
+      <p>Сникер - Магазин</p>
+      <div>
+        <p>Главная</p>
+        <p>Корзина</p>
+        <p>Контакты</p>
+      </div>
+    </div>
+  );
+}
+export default Header;
